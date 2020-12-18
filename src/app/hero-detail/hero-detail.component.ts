aa1af66d281f5893df490383e21df89b7ec8a621
ericmcgrandle <ericmcgrandle@gmail.com>
2020-12-18 15:06:40 -0800

    navigation

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 
 import { Hero } from '../hero';
+
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -11,9 +16,26 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute, // holds information about the route to this instance of the HeroDetailComponent
+    private HeroService: HeroService,
+    private location: Location // Angular's service for interacting with the browser.
+  ) { }
 
   ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    // route.snapshot is a static image of the route information shortly after the component was created.
+    // paramMap is a dictionary of route parameter values extracted from the URL
+    // + converts string to number
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.HeroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,13 +10,19 @@ import { MessageService } from './message.service'
 })
 export class HeroService {
 
+  // This is a typical "service-in-service" scenario: you inject the MessageService into the HeroService which is injected into the HeroesComponent.
+  constructor(private messageService: MessageService) { }
+
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
-  // This is a typical "service-in-service" scenario: you inject the MessageService into the HeroService which is injected into the HeroesComponent.
-  constructor(private messageService: MessageService) { }
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
 
 
 }
